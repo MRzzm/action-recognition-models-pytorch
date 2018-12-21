79920382d37f40687a6868301f2e611dc3217329
MRzzm <zhangzhimeng1@gmail.com>
2018-12-21 16:50:57 +0800

    2018-12-21

--- 3DCNN/Res3D/Res3D.py
+++ 3DCNN/Res3D/Res3D.py
@@ -9,16 +9,16 @@ import torch.nn as nn
 import torch.nn.init as init
 
 class ResBlock(nn.Module):
-    def __init__(self, in_channel,out_channel, stride=1):
+    def __init__(self, in_channel,out_channel, spatial_stride=1,temporal_stride=1):
         super(ResBlock, self).__init__()
 
-        self.conv1 = nn.Conv3d(in_channel, out_channel,kernel_size=(3,3,3),stride=stride,padding=(1,1,1))
+        self.conv1 = nn.Conv3d(in_channel, out_channel,kernel_size=(3,3,3),stride=(temporal_stride,spatial_stride,spatial_stride),padding=(1,1,1))
         self.conv2 = nn.Conv3d(out_channel, out_channel,kernel_size=(3, 3, 3),stride=(1, 1, 1),padding=(1, 1, 1))
         self.bn1 = nn.BatchNorm3d(out_channel)
         self.bn2 = nn.BatchNorm3d(out_channel)
         self.relu = nn.ReLU()
-        if in_channel != out_channel or stride != 1:
-            self.down_sample=nn.Sequential(nn.Conv3d(in_channel, out_channel,kernel_size=1,stride=stride,bias=False),
+        if in_channel != out_channel or spatial_stride != 1 or temporal_stride != 1:
+            self.down_sample=nn.Sequential(nn.Conv3d(in_channel, out_channel,kernel_size=1,stride=(temporal_stride,spatial_stride,spatial_stride),bias=False),
                                            nn.BatchNorm3d(out_channel))
         else:
             self.down_sample=None
@@ -39,15 +39,15 @@ class Res3D(nn.Module):
         super(Res3D, self).__init__()
 
         self.conv1=nn.Conv3d(3,64,kernel_size=(3,7,7),stride=(1,2,2),padding=(1,3,3))
-        self.conv2=nn.Sequential(ResBlock(64,64),
+        self.conv2=nn.Sequential(ResBlock(64,64,spatial_stride=2),
                                  ResBlock(64, 64))
-        self.conv3=nn.Sequential(ResBlock(64,128,2),
+        self.conv3=nn.Sequential(ResBlock(64,128,spatial_stride=2,temporal_stride=2),
                                  ResBlock(128, 128))
-        self.conv4 = nn.Sequential(ResBlock(128, 256, 2),
+        self.conv4 = nn.Sequential(ResBlock(128, 256, spatial_stride=2,temporal_stride=2),
                                    ResBlock(256, 256))
-        self.conv5 = nn.Sequential(ResBlock(256, 512, 2),
+        self.conv5 = nn.Sequential(ResBlock(256, 512, spatial_stride=2,temporal_stride=2),
                                    ResBlock(512, 512))
-        self.avg_pool=nn.AvgPool3d(kernel_size=(1,14,14))
+        self.avg_pool=nn.AvgPool3d(kernel_size=(1,7,7))
         self.linear=nn.Linear(512,num_class)
 
     def forward(self, x):
